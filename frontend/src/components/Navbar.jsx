052bfd5fbab4e523533e8929a951b8d5715c041a
zannat1023 <129942709+zannat1023@users.noreply.github.com>
2023-08-28 17:01:40 +0600

    having issue in jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
         {name: "Why Prapti", icon:"help-circle-outline", dis:"translate-x-48"},
         {name: "Customer_Reviews", icon:"chatbox-ellipses-outline", dis:"translate-x-64"},
     ];
-    const [active, setActive] = useState(0)
+    const [active, setActive] = useState(0);
 
     return(
         <div className="container">
@@ -19,37 +19,40 @@ const Navbar = () => {
                 <h1><img src={praptiLogo} alt="Prapti" /></h1>
             </Link>
         
-    
-            <div className="bg-black max-h-[4.4rem] px-6 rounded-t-x1">
+            
+            <div className="bg-blue max-h-[6rem] px-6 rounded-t-xl">
                 <ul className="flex relative">
-                    <span className={`bg-rose-600 duration-500 ${Menus[active].dis} border-4 border-gray-900 h-16 w-16 absolute -top-5 rounded-full `}
+                    
+                    <span className={`bg-rose-600 duration-500 ${Menus[active].dis} border-4 border-white-900 h-16 w-16 absolute 
+                    -top-5 rounded-full`}
                     >
-                    <span className="='w-3.5 h-3.5 bg-transparent absolute top-4 -left-[18px] rounded-tr-[11px]
+                    <span className="w-3.5 h-3.5 bg-transparent absolute top-4 -left-[18px] rounded-tr-[11px]
                     shadow-myShadow1"></span>
                     
 
-                    <span className="='w-3.5 h-3.5 bg-transparent absolute top-4 -right-[18px] rounded-tl-[11px]
+                    <span className="w-3.5 h-3.5 bg-transparent absolute top-4 -right-[18px] rounded-tl-[11px]
                     shadow-myShadow2"></span>
                     </span>
 
-                    {Menus.map((menus, i) => (
-                        <li key={i} className="w-16">
+                    {Menus.map((menu, i) => (
+                        <li key={i} className="w-20">
                             <a
                                 className="flex flex-col text-center pt-6"
                                 onclick={() => setActive(i)
                                 }
                             >
                                 <span
-                                    className={`text-s1 cursor-pointer duration-500 ${i === active && "-mt-6"}`}
+                                    className={`text-x1 cursor-pointer duration-500 ${i === active && "-mt-6 text-white"}`}
                                 >
-                                    <ion-icon name={menus.icon}></ion-icon>
+                                    <ion-icon name={menu.icon}></ion-icon>
                                 </span>
                                 <span
-                                    className={` ${active === i
+                                    className={` ${
+                                        active === i
                                             ? "translate-y-4 duration-700 opacity-100"
                                             : "opacity-0 translate-y-10"}`}
                                 >
-                                    {menus.name}
+                                    {menu.name}
                                 </span>
                             </a>
                         </li>
