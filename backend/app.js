2958fb7a145cee3740f19f29ba96366255938c7d
Arjun Karmaker <arjunkarmaker03@gmail.com>
2023-09-26 03:43:10 +0600

    expanded the user model

--- backend/app.js
+++ backend/app.js
@@ -10,14 +10,12 @@ const app = express();
 const ErrorHandler = require('./utilities/ErrorHandler');
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
-const fileUpload = require("express-fileupload");
 
 //middleware
 app.use(express.json());
 
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(fileUpload({ useTempFiles: true }));
 
 //error handling
 app.use(ErrorHandler);
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -4,24 +4,88 @@ const Schema = mongoose.Schema;
 
 const validator = require('validator');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const saltRounds = 10;
 
 const userSchema = new Schema({
     email: {
         type: String,
-        required: true,
+        required: [true, "Please enter your Email Address"],
         unique: true
     },
     username: {
         type: String,
-        required: true,
+        required: [true, "Please enter a Username"],
         unique: true
     },
     password: {
         type: String,
-        required: true
-    }
+        required: [true, "Please enter your Password"],
+        minLength: [4, "Password too short!"],
+        select: false
+    },
+    name: {
+        type: String,
+        required: [true, "Please enter Your Name"]
+    },
+    phoneNumber: {
+        type: Number
+    },
+    billingAddresses: [
+        {
+            country: {
+                type: String
+            },
+            city: {
+                type: String
+            },
+            address: {
+                type: String
+            },
+            postCode: {
+                type: Number
+            }
+        }
+    ],
+    role: {
+        type: String,
+        default: "user"
+    },
+    profilePic: {
+        public_id: {
+            type: String,
+            required: true
+        },
+        url: {
+            type: String,
+            required: true
+        }
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    },
+    resetPasswordToken: String,
+    resetPasswordTime: Date
 });
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+        next();
+    }
+    
+    this.password = await bcrypt.hash(this.password, saltRounds);
+});
+
+userSchema.methods.comparePassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
+/*
+userSchema.methods.getJwtToken = function () {
+    return jwt.sign({ id: this._id }, process.env.SECRET, { expiresIn: process.env.JWT_EXPIRES });
+};
+*/
 
 //static signup method
 userSchema.statics.signup = async function(email, username, password) {
